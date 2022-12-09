1f88ee8e032247ca396755d47ad8244796eaba8b
vazifa 3 <muminovdev@gmail.com>
2022-12-09 07:02:48 +0500

    Initial Commit

---
+++ dars 9.12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dars 9.12
+//
+//  Created by Mac on 09/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ dars 9.12/first view/FirdtViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirdtViewController.swift
+//  dars 9.12
+//
+//  Created by Mac on 09/12/22.
+//
+
+import UIKit
+
+class FirdtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
